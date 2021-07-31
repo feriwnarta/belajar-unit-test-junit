5c2c82bd758e582826f641cc8a10184fc8910192
feriwnarta <feriwnarta26@gmail.com>
2021-07-31 18:36:42 +0700

    menggunakan enable dn disable on jre, dan jre-range

--- src/test/java/ferdev/mvn/test/TestKondisi.java
+++ src/test/java/ferdev/mvn/test/TestKondisi.java
@@ -1,9 +1,8 @@
 package ferdev.mvn.test;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.*;
 
 public class TestKondisi {
 
@@ -18,4 +17,21 @@ public class TestKondisi {
             OS.LINUX
     })
     public void testDisableUntukOsLinux(){} // tes akan disable untuk os tertentu
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_16)
+    public void testEnableUntukJre16(){} // tes berhasil hanya untuk jre 16
+
+    @Test
+    @DisabledOnJre(JRE.JAVA_16)
+    public void testDisableUntukJre16(){} // tes gagal untuk jre 16
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_16)
+    public void testEnableOnJreRange8to16(){} // enable jre range 8-16
+
+    @Test
+    @DisabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_16)
+    public void testDisableOnJreRange8to16(){} // disable jre range 8-16
+
 }
