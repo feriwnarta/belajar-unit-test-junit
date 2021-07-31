46326aa9339ba3252a0eb04d723ee688f4e1a53a
feriwnarta <feriwnarta26@gmail.com>
2021-07-31 18:26:08 +0700

    tes kondisi untuk os tertentu

--- src/test/java/ferdev/mvn/test/TestKondisi.java
+++ src/test/java/ferdev/mvn/test/TestKondisi.java
@@ -1,4 +1,21 @@
 package ferdev.mvn.test;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
 public class TestKondisi {
+
+    @Test
+    @EnabledOnOs({
+            OS.LINUX
+    })
+    public void tesEnableUntukOsLinux(){}  // tes akan berjalan untuk os yg kita tentukan
+
+    @Test
+    @DisabledOnOs({
+            OS.LINUX
+    })
+    public void testDisableUntukOsLinux(){} // tes akan disable untuk os tertentu
 }
