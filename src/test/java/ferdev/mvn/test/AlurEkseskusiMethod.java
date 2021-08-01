2a4f651789b2a20ac90896deba268e5aaf818088
feriwnarta <feriwnarta26@gmail.com>
2021-08-01 20:00:01 +0700

    mengubah alur ekseskusi test dengan anotation @TestInstance menjadi per kelas, sehingga objek terus hidup dan dapat menggunakan 1 objek untuk seluruh method tst

--- src/test/java/ferdev/mvn/test/AlurEkseskusiMethod.java
+++ src/test/java/ferdev/mvn/test/AlurEkseskusiMethod.java
@@ -4,25 +4,45 @@ import org.junit.jupiter.api.*;
 
 // MethodOrder.random - > random
 // MethodOrderer.Orderanoation -> terprediksi harus menggunakan tambahan anotation Order di unit test mhetodnya
+// @TestInstance(TestInstance.Lifecycle.PER_METHOD) // default lifecycle unit test
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // mengubah lifecycle test menjadi kelas sehingga objek akan terus hidup
+                                                // dan dapat menggunakan 1 objek untuk seluruh method unit test
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AlurEkseskusiMethod {
+    private int counter;
+
     @Order(1)
     @Test
-    public void test5(){}
+    public void test5(){
+        counter++;
+        System.out.println(counter);
+    }
 
     @Order(5)
     @Test
-    public void test2(){}
+    public void test2(){
+        counter++;
+        System.out.println(counter);
+    }
     @Test
 
     @Order(2)
-    public void test3(){}
+    public void test3(){
+        counter++;
+        System.out.println(counter);
+    }
     @Test
 
     @Order(3)
-    public void test1(){}
+    public void test1(){
+        counter++;
+        System.out.println(counter);
+    }
 
     @Order(4)
     @Test
-    public void test4(){}
+    public void test4(){
+        counter++;
+        System.out.println(counter);
+    }
 }
