b8d1195a4df9ae724b37b9ff2811d6c1e2251342
feriwnarta <feriwnarta26@gmail.com>
2021-08-01 19:32:04 +0700

    method orderer kelar

--- src/test/java/ferdev/mvn/test/AlurEkseskusiMethod.java
+++ src/test/java/ferdev/mvn/test/AlurEkseskusiMethod.java
@@ -1,18 +1,28 @@
 package ferdev.mvn.test;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+// MethodOrder.random - > random
+// MethodOrderer.Orderanoation -> terprediksi harus menggunakan tambahan anotation Order di unit test mhetodnya
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AlurEkseskusiMethod {
+    @Order(1)
     @Test
     public void test5(){}
+
+    @Order(5)
     @Test
     public void test2(){}
     @Test
+
+    @Order(2)
     public void test3(){}
     @Test
+
+    @Order(3)
     public void test1(){}
+
+    @Order(4)
     @Test
     public void test4(){}
 }
