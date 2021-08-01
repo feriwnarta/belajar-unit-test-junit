c18078b21931228135f9c8dd73c7909268369b8a
feriwnarta <feriwnarta26@gmail.com>
2021-08-01 19:24:56 +0700

    menggunakan test method order

--- src/test/java/ferdev/mvn/test/AlurEkseskusiMethod.java
+++ src/test/java/ferdev/mvn/test/AlurEkseskusiMethod.java
@@ -1,4 +1,18 @@
 package ferdev.mvn.test;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
 public class AlurEkseskusiMethod {
+    @Test
+    public void test5(){}
+    @Test
+    public void test2(){}
+    @Test
+    public void test3(){}
+    @Test
+    public void test1(){}
+    @Test
+    public void test4(){}
 }
