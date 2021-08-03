b56cad2d81f8ea7abc491e7b8faed448241b793b
feriwnarta <feriwnarta26@gmail.com>
2021-08-03 18:31:52 +0700

    nested test / inner test kelar

--- src/test/java/ferdev/mvn/test/NestedClassTest.java
+++ src/test/java/ferdev/mvn/test/NestedClassTest.java
@@ -1,4 +1,35 @@
 package ferdev.mvn.test;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@DisplayName("Sebuah Nested Class")
 public class NestedClassTest {
+
+    @DisplayName("Memiliki tst1, untuk inisialisasi")
+    @Test
+    public void test1(){
+
+    }
+
+    @Nested
+    @DisplayName("Dilanjutkan ke inner kelas")
+    public class Iner {
+
+        @DisplayName("Memiliki tst2, dari Inner")
+        @Test
+        public void test2(){
+
+        }
+
+        @DisplayName("Cetak kesimpulan dari Inner class")
+        @Test
+        public void test3(){
+
+        }
+    }
+
+
 }
