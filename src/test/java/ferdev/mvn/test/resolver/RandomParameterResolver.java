15743721bc8dd61052a1e197d78d727d8fe20427
feriwnarta <feriwnarta26@gmail.com>
2021-08-03 19:20:23 +0700

    random parameter resolver kelar

--- src/test/java/ferdev/mvn/test/resolver/RandomParameterResolver.java
+++ src/test/java/ferdev/mvn/test/resolver/RandomParameterResolver.java
@@ -2,6 +2,10 @@ package ferdev.mvn.test.resolver;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
 
 import java.util.Random;
 
@@ -11,7 +15,17 @@ import java.util.Random;
 // akan langsung disi oleh pemanggil method (method call), berbeda dengan method unit test karena
 // langsung jalan dan gaada yg mengisi parameter, maka dibutuhkan parameter resolver atau dependency injection
 // secara otomatis akan dibuatkan objek oleh parameter resolver dan dapat langsung digunakan oleh method test
-@DisplayName("A random resolver")
-public class RandomParameterResolver {
 
+@DisplayName("A parameter random resolver")
+public class RandomParameterResolver implements ParameterResolver {
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType() == Random.class;
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return new Random();
+    }
 }
